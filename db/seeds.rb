14331693955e1454957854e258292ab6ac5554ce
git-kaz <okw.kzk@gmail.com>
2025-11-17 11:12:22 +0900

    add:migrate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,38 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Topic.create!([
+
+#アイスブレイク系
+{ content: "好きな食べ物は？", category: "icebreak" },
+{ content: "最近見たテレビやYOUTUBEでおすすめは？", category: "icebreak" },
+{ content: "休日は何して過ごしている？", category: "icebreak" },
+{ content: "好きな音楽のジャンルは？", category: "icebreak" },
+{ content: "行ってみたい旅行先は？", category: "icebreak" },
+{ content: "最近ハマっていることは？", category: "icebreak" },
+{ content: "好きな食べ物・苦手な食べ物は？", category: "icebreak" },
+{ content: "どんなところに住んでる？", category: "icebreak" },
+{ content: "普段なんて呼ばれてる？", category: "icebreak" },
+{ content: "お酒は飲む？飲むなら何が好き？", category: "icebreak" },
+
+#真面目系
+{ content: "仕事で大切にしていることは？", category: "serious" },
+{ content: "将来の目標について聞かせてください", category: "serious" },
+{ content: "最近学んだことはありますか？", category: "serious" },
+{ content: "尊敬する人物とその理由は？", category: "serious" },
+{ content: "困難を乗り越えたエピソードはありますか？", category: "serious" },
+{ content: "人生で大切にしている価値観は？", category: "serious" },
+{ content: "普段の勉強方法を教えてください", category: "serious" },
+{ content: "これから学んでみたいことは何？", category: "serious" },
+{ content: "感銘を受けた本はありますか？", category: "serious" },
+{ content: "今後学びたいことはなんですか？", category: "serious" },
+
+#尖った系
+{ content: "今までに食べたパンの枚数は？", category: "joke" },
+{ content: "「俺でなきゃ見逃しちゃうね」と思ったことは？", category: "joke" },
+{ content: "クセになってることは？（例：音を殺して歩く）", category: "joke" },
+{ content: "奇跡は一度だけ？じゃあ二度目は何ですか？", category: "joke" },
+{ content: "そこにしびれる憧れると思ったことはなんですか？", category: "joke" },
+{ content: "落ち着きたい時には何をしますか？（例：素数を数える）", category: "joke" },
+{ content: "最近の要チェックや！なことは？", category: "joke" },
+{ content: "ヤムチャが天下一武道会で一回戦を突破したくらいありえない出来事はありますか？", category: "joke" },
+{ content: "ありのまま今起こったことを話してください", category: "joke" },
+{ content: "感謝っ圧倒的感謝だったことは？", category: "joke" }
+])
